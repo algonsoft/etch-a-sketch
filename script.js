ba96a89b35b666a907c2fe9e4aa1f656342cca1b
algonsoft <smtalgonsoft@gmail.com>
2021-12-17 16:56:05 +1030

    Set up files for project

---
+++ script.js
@@ -0,0 +1,6 @@
+const container = document.querySelector(`#container`)
+
+const content = document.createElement(`div`);
+content.classList.add('content');
+content.style.cssText = "color: blue";
+container.appendChild(content);
