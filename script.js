c16dddb24bb2bd5e8c512de5671e66b3dc83fdb0
algonsoft <smtalgonsoft@gmail.com>
2021-12-28 18:52:15 +1030

    Functionally ok, need to fix height.

--- script.js
+++ script.js
@@ -1,6 +1,89 @@
-const container = document.querySelector(`#container`)
-
-const content = document.createElement(`div`);
+const container = document.querySelector('.container');
+const container2 = document.querySelector('container2')
+let size = 0;
+for (let i = 0; i < 256; i++) {
+const content = document.createElement('div');
 content.classList.add('content');
-content.style.cssText = "color: blue";
+content.style.width = "110px";
 container.appendChild(content);
+}
+
+
+let click = document.querySelectorAll('button')
+
+click.forEach((button) => {
+  
+
+ button.addEventListener('click', () => {
+   
+      size = prompt("Enter Grid Size (z * z)");
+      
+      if (size >= 1 && size <= 100 ){
+        container.replaceChildren();
+        let newWidth = 1760 / size;
+        
+
+        for (i = 1; i <= size * size; i++) {   
+          content = document.createElement('div');
+          content.classList.add('content');
+          content.style.width = newWidth + "px";
+          container.appendChild(content);
+        }
+       refreshedGrid();
+       
+       
+
+      }
+     
+        
+      
+  
+
+ });
+});
+
+
+
+
+let test  = document.querySelectorAll('.content')
+test.forEach((div) => {
+
+function backgroundUpdate(){
+  div.style.cssText = "background-color: white";
+}
+
+function backgroundRevert(){
+  div.style.cssText = "background-color: peachpuff";
+
+}
+
+div.addEventListener('mouseover', backgroundUpdate);
+div.addEventListener('mouseout', backgroundRevert);
+
+});
+
+function refreshedGrid(){
+let testtest  = document.querySelectorAll('.content')
+testtest.forEach((div) => {
+  let newWidth = 1760 / size;
+
+function backgroundUpdate2(){
+div.style.cssText = "background-color: white";
+div.style.width = newWidth + "px";
+
+}
+
+function backgroundRevert2(){
+div.style.cssText = "background-color: peachpuff";
+div.style.width = newWidth + "px";
+
+}
+
+div.addEventListener('mouseover', backgroundUpdate2);
+div.addEventListener('mouseout', backgroundRevert2);
+
+});
+}
+
+
+
